7ac3f120a7a00924fc62463fbaa09f0eb74d6496
Crynotsad <crynotsadpvp@gmail.com>
2026-01-20 10:11:26 +0700

    Nguyen dinh phong commit

--- bai1.c
+++ bai1.c
@@ -20,12 +20,19 @@
 int main(){
 
     // Khai báo biến
-
+    float diem;
 
     // Nhập dữ liệu
-
-
+    printf("Xét học lưc\n");
+    printf("Nhập điểm:");
+    scanf("%f", &diem);
     // Xử lý, tính toán VÀ Hiển thị kết quả
-
+    if (diem<=0||diem>=10){printf("Điểm không hợp lệ");
+    }else if(diem>=9){printf("Học lực xuất sắc");
+    }else if(diem<9&&diem>=8){printf("Học lưc giỏi");
+    }else if(diem<8&&diem>=6.5){printf("Học lực khá");
+    }else if(diem<6.5&&diem>=5){printf("Học lực trung bình");
+    }else if(diem<5&&diem>=3.5){printf("Học lực yếu");
+    }else if(diem<3.5){printf("Học lực kém");}
     return 0;
 }
--- bai3.c
+++ bai3.c
@@ -13,11 +13,22 @@
 int main(){
     
     // Khai báo biến
-
+    int soDien;
+    float tienDien;
 
     // Nhập dữ liệu
-
+    printf("Nhập số điện tiêu thụ:");
+    scanf("%d",&soDien);
+    
 
     // Xử lý, tính toán VÀ Hiển thị kết quả
-
+    if(soDien <= 50) {
+        tienDien = soDien * 1000;
+    } else if(soDien <= 100) {
+        tienDien = 50 * 1000 + (soDien - 50) * 1200;
+    } else {
+        tienDien = 50 * 1000 + 50 * 1200 + (soDien - 100) * 1500;
+    }
+    printf("Số tiền điện phải trả là: %.2f\n", tienDien);
+    return 0;
 }
